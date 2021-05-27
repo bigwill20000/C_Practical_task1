8202f931f672c6b724f2b1169ff73f8f70bf98e1
bigwill20000 <bigwill20000@gmail.com>
2021-05-27 22:06:22 +0200

    latest changes with Emmanuel

--- includes/attendance.h
+++ includes/attendance.h
@@ -32,19 +32,28 @@ typedef struct student
     char firstName[100];
     char lastName[100];
     int matriculationNumber;
-    DEGREE_PROGRAM *program;
-    COURSE  course; //coursesAttended;
+    DEGREE_PROGRAM *degree;
+    COURSE**  courses; //linked list of course pointers;
     STUDENT *next;
 }STUDENT;
 
 typedef struct instructor
 {
+    int id;
     char firstName[100];
     char lastName[100];
     char grade[100];
     char faculty[100];
-    COURSE course;
+    COURSE** courses;
     INSTRUCTOR *next;
 }INSTRUCTOR;
+
+
+INSTRUCTOR hire_instructor(int id, char* first_name, char* last_name, char* grade, char* faculty);
+INSTRUCTOR* add_instructor(INSTRUCTOR instructor, INSTRUCTOR* list);
+STUDENT register_student(int id, char* first_name, char* last_name, int matriculation, DEGREE_PROGRAM* degree);
+STUDENT* add_student(STUDENT student, STUDENT* list);
+COURSE create_course(int id, char* name, SEMESTER semester);
+COURSE* add_course(COURSE course, COURSE* list);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -5,6 +5,15 @@
 
 int main()
 {
+    //Declare link lists
+    STUDENT* students = NULL;
+    INSTRUCTOR* instructors = NULL;
+    COURSE* courses = NULL;
+
+    //Initialize linked lists
+    students = (STUDENT*) (malloc(sizeof(STUDENT) * 3));
+    instructors = (INSTRUCTOR*) (malloc(sizeof(INSTRUCTOR) * 3));
+
     // MAIN PROGRAM HERE
 
     return 0;
