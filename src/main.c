5620060e4aedcc204f393d76b824f41003d6e07e
bigwill20000 <bigwill20000@gmail.com>
2022-03-08 15:38:18 +0100

    beginning of main 08/03/2022

--- src/main.c
+++ src/main.c
@@ -9,15 +9,40 @@
 int main()
 {
     //Declare link lists
-    STUDENT* students = NULL;
-    INSTRUCTOR* instructors = NULL;
-    DEGREEPROGRAM *degreeProgram = NULL;
+    STUDENT* studentsList = NULL;
+    INSTRUCTOR* instructorsList = NULL;
+    DEGREEPROGRAM *degreeProgramList = NULL;
+    
+    COURSE course;
+    INSTRUCTOR instructor;
+    DEGREEPROGRAM degreeProgram;
+    STUDENT student;
 
+    
     //Initialize linked lists
-    students = (STUDENT*) (malloc(sizeof(STUDENT) * 3));
-    instructors = (INSTRUCTOR*) (malloc(sizeof(INSTRUCTOR) * 3));
+    //students = (STUDENT*) (malloc(sizeof(STUDENT) * 3));    
+    //instructors = (INSTRUCTOR*) (malloc(sizeof(INSTRUCTOR) * 3));
+    //degreeProgram = (DEGREEPROGRAM*) (malloc(sizeof(DEGREEPROGRAM) * 3));
+
 
     // MAIN PROGRAM HERE
 
     return 0;
+}
+
+void start_Program(int *choice)
+{
+    printf("\t\tMENU\t\t\n");
+    printf("\t\t====\t\t\n");
+    printf("1 - Add Student\n");
+    printf("2 - Add Instructors\n");
+    printf("3 - Add Degree Program\n");
+    printf("4 - Retrieve all Courses");
+    printf("5 - Retrieve Students' Infos");
+    printf("6 - Retrieve Students' Names");
+    printf("7 - Exit\n");
+    printf("\t\t====\t\t\n");
+    printf("Input your choice : ");
+    scanf("%d",choice);
+    printf("\n");
 }
